e086d2b3c401d2a4064044a5d1eac88b446c23d8
UserName101-md <danielegarciaa3>
2025-02-01 13:41:12 -0600

    Actividad 3/ Finalización de la interfaz y correción de errores en la logíca interna

--- src/main/java/org/daniel/alduenda/actividadez/actividad3/user_int/CLI.java
+++ src/main/java/org/daniel/alduenda/actividadez/actividad3/user_int/CLI.java
@@ -13,28 +13,33 @@ public class CLI {
         Scanner scanner = new Scanner(System.in);
         ProductManager productManager = new ProductManager();
         System.out.println("""
-            [v]Tienda de Dani:
-            ---------------------------------
-            Selecciona una opción
-            
-            [@]x. Agregar un producto al carrito. -
-            [Q]2. (No disponible). -
-            ---------------------------------
-            Opciones:
-            [@]3. Salir
-            """);
+            ╔═════════════════════════════════════════════════════════════════════╗
+            ║ [v] Tienda de Dani:                                                 ║
+            ╠═════════════════════════════════════════════════════════════════════╣
+            ║ [:] Selecciona una opción                                           ║
+            ╟┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╢
+            ║                                                                     ║
+            ║ [@] x. Agregar un producto al carrito. -                            ║
+            ║ [Q] 2. (No disponible). -                                           ║
+            ╠═════════════════════════════════════════════════════════════════════╣
+            ║ [*] Opciones:                                                       ║
+            ║    [@]3. Salir                                                      ║
+            ╚═════════════════════════════════════════════════════════════════════╝
+           """);
         int option = scanner.nextInt();scanner.nextLine();
 
 
         while (option != 3){
             switch (option){
                 case 1:
-                    System.out.print("[*] Bienvenido a su carrito, Por favor ingrese la información de su producto. - \n");
-                    System.out.println("---------------------------------");
-                    System.out.print("[?] Ingrese el nombre del producto: \n");
+                    System.out.println("╔══════════════════════════════════════════════════════════════════════════════════════╗");
+                    System.out.println("║  [*] Bienvenido a su carrito, Por favor ingrese la información de su producto. - \n  ║");
+                    System.out.println("╠══════════════════════════════════════════════════════════════════════════════════════╣");
+                    System.out.println("║  [?] Ingrese el nombre del producto: \n                                              ║");
                     String name = scanner.nextLine();
-                    System.out.println("---------------------------------");
-                    System.out.print("[?] Ingrese el precio del producto: \n");
+                    System.out.println("╠══════════════════════════════════════════════════════════════════════════════════════╣");
+                    System.out.println("║  [?] Ingrese el precio del producto: \n                                              ║");
+                    System.out.println("╚══════════════════════════════════════════════════════════════════════════════════════╝");
                     double cost = scanner.nextDouble();
                     double beforeTax = cost + (cost*.20);
                     double afterTax = beforeTax + (beforeTax*.162);
@@ -66,15 +71,18 @@ public class CLI {
      */
     public static void showMenu(){
         System.out.println("""
-            [v]Tienda de Dani:
-            ---------------------------------
-            Selecciona una opción
-            
-            [@]1. Agregar un producto al carrito. -
-            [Q]2. Comparar precios del carrito. -
-            ---------------------------------
-            Opciones:
-            [@]3. Salir
+           ╔═════════════════════════════════════════════════════════════════════╗
+           ║ [v] Tienda de Dani:                                                 ║
+           ╠═════════════════════════════════════════════════════════════════════╣
+           ║ [:] Selecciona una opción                                           ║
+           ╟┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╢
+           ║                                                                     ║
+           ║ [@] 1. Agregar un producto al carrito. -                            ║
+           ║ [Q] 2. (No disponible). -                                           ║
+           ╠═════════════════════════════════════════════════════════════════════╣
+           ║ [*] Opciones:                                                       ║
+           ║    [@]3. Salir                                                      ║
+           ╚═════════════════════════════════════════════════════════════════════╝
             """);
     }
 
@@ -83,10 +91,11 @@ public class CLI {
      * @param product el objeto principal, incluye nombre, costo e impuestos. -
      */
     public static void formatProduct(Product product){
-        System.out.println("\n" +
-                "[#] Nombre del producto: " + product.getName() + '\n' +
-                "[$] Precio: " + product.getCost() + '\n' +
-                "[<] Precio previo a impuestos: " + product.getBeforeTax() + '\n' +
-                "[>] Precio después de impuestos: " + product.getAfterTax() + '\n' );
+        System.out.println("╔═════════════════════════════════════════════════════════════════════╗\n" +
+                           "║ [#] *Nombre del producto: " + product.getName() + "                 ║\n" +
+                           "║ [$] Precio: " + product.getCost() + "                               ║\n" +
+                           "║ [<] Precio previo a impuestos: " + product.getBeforeTax() + "       ║\n" +
+                           "║ [>] Precio después de impuestos: " + product.getAfterTax() + "      ║\n"  +
+                           "╚═════════════════════════════════════════════════════════════════════╝");
     }
 }
